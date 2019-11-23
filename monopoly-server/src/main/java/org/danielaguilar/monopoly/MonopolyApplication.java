8bb124aeb227b19da4ab60c93deaa2c80dabb511
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2019-11-23 13:40:39 -0600

    Enable CORS on Angular's development server

--- monopoly-server/src/main/java/org/danielaguilar/monopoly/MonopolyApplication.java
+++ monopoly-server/src/main/java/org/danielaguilar/monopoly/MonopolyApplication.java
@@ -14,3 +14,13 @@ public class MonopolyApplication {
 
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+}
