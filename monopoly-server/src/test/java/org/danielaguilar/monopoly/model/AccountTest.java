1bb3ee6ce650c5d64eece445d51d22775a044af1
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2019-11-23 13:38:14 -0600

    Add AccountTest

---
+++ monopoly-server/src/test/java/org/danielaguilar/monopoly/model/AccountTest.java
@@ -0,0 +1,29 @@
+package org.danielaguilar.monopoly.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+
+	private Account account;
+
+	@BeforeEach
+	public void setUp() {
+		account = new Account();
+		account.setBalance(100);
+	}
+
+	@Test
+	public void testWithdrawBalance() {
+		account.withdraw(80);
+		assertEquals(20, account.getBalance());
+	}
+
+	@Test
+	public void testNegativeBalance() {
+		assertThrows(IllegalArgumentException.class, () -> account.withdraw(110));
+	}
+}
