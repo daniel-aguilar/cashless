8f298c5d6c2c45b83fc5f5c1365f88ef543e4fb1
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-07-29 23:42:38 -0600

    Extract join game logic to base class

---
+++ client/src/app/auth/gateway.ts
@@ -0,0 +1,36 @@
+import { Directive } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GameService } from 'src/app/game.service';
+import { LoadingService } from 'src/app/loading/loading.service';
+import { AuthService } from './auth.service';
+
+@Directive()
+export class Gateway {
+
+  constructor(
+    private router: Router,
+    private game: GameService,
+    private auth: AuthService,
+    private loading: LoadingService,
+    /* HACK: Adding all services here so I won't have
+       to declare them in every child constructor */
+    protected fb: FormBuilder) {
+
+  }
+
+  enterGame(pin: string) {
+    this.loading.show();
+    return this.game.joinGame(pin).toPromise()
+        .then(() => {
+          const account = this.auth.getLoggedAccount();
+
+          if (account.isBanker) {
+            this.router.navigateByUrl('/banker');
+          } else {
+            this.router.navigateByUrl('/player');
+          }
+        })
+        .finally(() => this.loading.hide());
+  }
+}
--- client/src/app/auth/join-game/join-game.component.ts
+++ client/src/app/auth/join-game/join-game.component.ts
@@ -1,50 +1,30 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { GameService } from 'src/app/game.service';
-import { LoadingService } from 'src/app/loading/loading.service';
-import { AuthService } from '../auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { Gateway } from '../gateway';
 
 @Component({
   selector: 'app-join-game',
   templateUrl: './join-game.component.html',
   styleUrls: ['./join-game.component.scss']
 })
-export class JoinGameComponent {
+export class JoinGameComponent extends Gateway implements OnInit {
   form: FormGroup;
   error = false;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private game: GameService,
-    private auth: AuthService,
-    private loading: LoadingService) {
-
+  ngOnInit() {
     this.form = this.fb.group({
       pin: ['', Validators.required],
     });
   }
 
-  joinGame() {
+  async joinGame() {
     const pin = this.form.value.pin as string;
-    this.loading.show();
-    this.game.joinGame(pin).subscribe({
-      complete: () => {
-        this.loading.hide();
-        const account = this.auth.getLoggedAccount();
-
-        if (account.isBanker) {
-          this.router.navigateByUrl('/banker');
-        } else {
-          this.router.navigateByUrl('/player');
-        }
-      },
-      error: () => {
-        this.loading.hide();
-        this.displayError();
-      },
-    });
+    try {
+      await this.enterGame(pin);
+    }
+    catch (error) {
+      this.displayError();
+    }
   }
 
   private displayError() {
