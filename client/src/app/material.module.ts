6133f6aa582be1b373942b8ed5624af74c79b4bb
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2022-09-05 21:13:16 -0600

    Replace Font Awesome with Material Icons

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -18,6 +19,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatDividerModule,
     MatDialogModule,
     MatProgressSpinnerModule,
+    MatIconModule,
   ],
   exports: [
     MatButtonModule,
@@ -28,6 +30,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatDividerModule,
     MatDialogModule,
     MatProgressSpinnerModule,
+    MatIconModule,
   ],
   providers: [
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } }
