f0c45dbb048ba868f8b544472844b43ac52a6c38
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2024-03-22 20:54:11 -0600

    Prefer JpaRepository to CrudRepository

--- src/main/java/org/danielaguilar/cashless/repository/TransactionRepository.java
+++ src/main/java/org/danielaguilar/cashless/repository/TransactionRepository.java
@@ -1,19 +1,17 @@
 package org.danielaguilar.cashless.repository;
 
-import java.util.List;
-
 import org.danielaguilar.cashless.model.Account;
 import org.danielaguilar.cashless.model.Game;
 import org.danielaguilar.cashless.model.Transaction;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TransactionRepository
-		extends CrudRepository<Transaction, Integer>, PagingAndSortingRepository<Transaction, Integer> {
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
 	@Query("FROM Transaction "
          + "WHERE sender = ?1 OR recipient = ?1 "
