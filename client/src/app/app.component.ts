a90deabb44549d86fbf3d2b537b0ec0919a9bb05
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2024-06-14 01:20:58 -0600

    Move theming to single file, remove elevation
    Do away with partials (i.e. _variables.scss), replace styling from
    raised (elevation) to flat.
    
    Also:
    
    * app-toolbar: Use proper mat-icon-button, remove fontIcon property
    * Remove Material 2 type scale classes: This commit was originally a
      (now failed) attempt to migrate the theme to Material 3. I will
      revisit this in the eventual migration.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   account: Account;
