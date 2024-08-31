0a705bc1891dd2bf1303c0c61a030a57a751e0d2
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2024-08-31 11:43:21 -0600

    Add action to ping wake-up endpoint.
    (Doesn't actually) Revert "Add fn_prevent_pausing and wake-up endpoint"
    
    This reverts commit 63f1abe1806694781b7a5b4f24c08116d0dab05f.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
-import { environment as env } from 'src/environments/environment';
 import { Account } from './auth/account';
 import { AuthService } from './auth/auth.service';
 
@@ -31,10 +30,6 @@ export class AppComponent implements OnInit {
   leaveGame() {
     this.auth.logout();
     this.router.navigateByUrl('/');
-  }
-
-  wakeUp() {
-    location.assign(`${env.apiURL}/sb/wake-up`);
   }
 
   private listenForBankerRoute() {
